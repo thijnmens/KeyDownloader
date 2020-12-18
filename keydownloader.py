a566330b13e94e2633c29d1af46990f4be389418
thijnmens <57702588+thijnmens@users.noreply.github.com>
2020-12-18 22:13:19 +0100

    Fixed Insane Memory Usage

--- keydownloader.py
+++ keydownloader.py
@@ -2,26 +2,49 @@
 # Operation Steal BeatSavers Database #
 #######################################
 #      Created by: Thijnmens          #
-#         Version: 1.0.1              #
+#         Version: 1.1.1              #
 #######################################
 
 import webbrowser
 import time
 import sys
+import os
 import psutil
+
+print('---------------')
+print('Running version 1.1.1')
+print('Created by Thijnmens, pls don\' DDoS BeatSaver with this script thank you very much')
+print('---------------')
+print('Checking dependencies, please wait')
+
+modul = 'psutil'
+if modul not in sys.modules:
+    print('---------------')
+    print('!!!YOU DO NOT HAVE PSUTIL INSTALLED!!!')
+    print('PLEASE INSTALL PSUTIL BY RUNNING THE FOLLOWING COMMAND')
+    print('vvvvvv')
+    print('pip install psutil')
+    print('^^^^^^')
+    print('WITHOUT THIS THE PROGRAM WILL CRASH')
+    print('---------------')
+else:
+    print('All dependecies are validated')
 
 count = int(input('Input the last reported count value here [Default: 0] : '))
 delay = int(input('Input the delay in seconds between each download [Default: 5] : '))
 maxim = int(input('Input the maximum amount of songs to download [Default: 72644] : '))
-rever = str(input('Do you want it to run backwards? Y/N : '))
-maxra = int(input('What is the maximum amount of ram that can be used by the system? : '))
+rever = str(input('Do you want it to run backwards? [Y/N] : '))
+maxra = int(input('What is the maximum amount of ram that can be used by the system? [0~100] : '))
 url = 'beatsaver://'
 
 if(rever == 'N'):
     while(count < maxim):
-        ram = psutil.virtual_memory().percent
+        pro = psutil.Process(os.getpid())
+        ram = pro.memory_percent()
         if(ram > maxra):
             print('Ram usage is too high, waiting 30 seconds')
+            print('Memory Usage: ', ram,'%')
+            print('Memory Left: ', maxra - ram,'%')
             time.sleep(30)
         else:
             count = count + 1
@@ -31,14 +54,19 @@ if(rever == 'N'):
             print('Starting Download of: ', key)
             print('url: ', url)
             print('count', count)
+            print('Memory Usage: ', ram,'%')
+            print('Memory Left: ', maxra - ram,'%')
             print('---------------')
             webbrowser.open(url)
             
 if(rever == 'Y'):
     while(maxim > count):
-        ram = psutil.virtual_memory().percent
+        pro = psutil.Process(os.getpid())
+        ram = pro.memory_percent()
         if(ram > maxra):
             print('Ram usage is too high, waiting 30 seconds')
+            print('Memory Usage: ', ram,'%')
+            print('Memory Left: ', maxra - ram,'%')
             time.sleep(30)
         else:
             maxim = maxim - 1
@@ -47,7 +75,9 @@ if(rever == 'Y'):
             url = 'beatsaver://' + key
             print('Starting Download of: ', key)
             print('url: ', url)
-            print('count', maxim)
+            print('count: ', maxim)
+            print('Memory Usage: ', ram,'%')
+            print('Memory Left: ', maxra - ram,'%')
             print('---------------')
             webbrowser.open(url)
 
