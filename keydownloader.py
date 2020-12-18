cb5235e61c822d2f8d682928f26c5706a7e4026c
thijnmens <57702588+thijnmens@users.noreply.github.com>
2020-12-18 21:29:02 +0100

    Temp fix for insane RAM usage
    Yes this thing used 90% of your ram no matter the amount of ram you had

--- keydownloader.py
+++ keydownloader.py
@@ -2,41 +2,54 @@
 # Operation Steal BeatSavers Database #
 #######################################
 #      Created by: Thijnmens          #
-#         Version: 1.1.0              #
+#         Version: 1.0.1              #
 #######################################
 
 import webbrowser
 import time
 import sys
+import psutil
 
 count = int(input('Input the last reported count value here [Default: 0] : '))
 delay = int(input('Input the delay in seconds between each download [Default: 5] : '))
 maxim = int(input('Input the maximum amount of songs to download [Default: 72644] : '))
 rever = str(input('Do you want it to run backwards? Y/N : '))
+maxra = int(input('What is the maximum amount of ram that can be used by the system? : '))
 url = 'beatsaver://'
 
 if(rever == 'N'):
     while(count < maxim):
-        count = count + 1
-        key = hex(count)[2:]
-        time.sleep(delay)
-        url = 'beatsaver://' + key
-        print('Starting Download of: ', key)
-        print('url: ', url)
-        print('count', count)
-        print('---------------')
-        webbrowser.open(url)
+        ram = psutil.virtual_memory().percent
+        if(ram > maxra):
+            print('Ram usage is too high, waiting 30 seconds')
+            time.sleep(30)
+        else:
+            count = count + 1
+            key = hex(count)[2:]
+            time.sleep(delay)
+            url = 'beatsaver://' + key
+            print('Starting Download of: ', key)
+            print('url: ', url)
+            print('count', count)
+            print('---------------')
+            webbrowser.open(url)
+            
 if(rever == 'Y'):
     while(maxim > count):
-        maxim = maxim - 1
-        key = hex(maxim)[2:]
-        time.sleep(delay)
-        url = 'beatsaver://' + key
-        print('Starting Download of: ', key)
-        print('url: ', url)
-        print('count', maxim)
-        print('---------------')
-        webbrowser.open(url)
+        ram = psutil.virtual_memory().percent
+        if(ram > maxra):
+            print('Ram usage is too high, waiting 30 seconds')
+            time.sleep(30)
+        else:
+            maxim = maxim - 1
+            key = hex(maxim)[2:]
+            time.sleep(delay)
+            url = 'beatsaver://' + key
+            print('Starting Download of: ', key)
+            print('url: ', url)
+            print('count', maxim)
+            print('---------------')
+            webbrowser.open(url)
 
 if(count == maxim):
     print('ALL SONGS HAVE BEEN DOWNLOADED!')
